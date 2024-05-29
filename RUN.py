a1288422577f86aa677077e2fe125b12da2d5cfb
BeemoNgo <104055130@student.swin.edu.au>
2024-05-29 17:32:01 +1000

    complete the main process

--- RUN.py
+++ RUN.py
@@ -1,4 +1,4 @@
-import sys
+import os
 from Table import Table
 from Reservation import Reservation
 from OnlineSystem import OnlineSystem
@@ -21,7 +21,12 @@ kitchen = KitchenOperation()
 OrderManagement.initialise_system()  # Initialize the tables and system
 factory = CustomerFactory(online_system)
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def main():
+    clear_screen()
     print("Welcome to the Relaxing Koala Restaurant")
     choice = input("You are: \n1. Customer\n2. Staff\n")
 
@@ -35,6 +40,7 @@ def main():
         main()
 
 def staff():
+    clear_screen()
     choice = input("You are: \n1. Front Staff\n2. Kitchen Staff\n3. Go back\n")
 
     if choice == "1":
@@ -48,19 +54,24 @@ def staff():
         staff()
 
 def front_staff():
+    clear_screen()
     print("Front Staff Register")
     while True:
         action = input("Select an option:\n1. Check table status\n2. Activate table\n3. View order status by table\n4. Process payment\n5. Go back\n")
         if action == "1":
+            clear_screen()
             online_system.check_table_status()
         elif action == "2":
             table_id = int(input("Enter table ID to activate: "))
+            clear_screen()
             online_system.activate_table(table_id)
         elif action == "3":
             table_id = int(input("Enter table ID to view order status: "))
+            clear_screen()
             online_system.get_order_status_by_table(table_id)
         elif action == "4":
             table_id = int(input("Enter table ID to process payment: "))
+            clear_screen()
             online_system.display_invoice_by_table_id(table_id)
             payment_method = input("Enter payment method (card/cash): ")
             amount = float(input("Enter the amount: "))
@@ -72,6 +83,7 @@ def front_staff():
             print("Invalid choice. Please enter a valid option.")
 
 def kitchen_staff():
+    clear_screen()
     print("Kitchen Operation")
     kitchen.display_all_order_statuses()
     while True:
@@ -79,10 +91,13 @@ def kitchen_staff():
         if action == "1":
             item_id = int(input("Enter item ID to start preparing: "))
             kitchen.start_preparing(item_id)
+            clear_screen()
         elif action == "2":
             item_id = int(input("Enter item ID to complete: "))
             kitchen.complete_item(item_id)
+            clear_screen()
         elif action == "3":
+            clear_screen()
             kitchen.display_all_order_statuses()
         elif action == "4":
             staff()
@@ -91,11 +106,13 @@ def kitchen_staff():
             print("Invalid choice. Please enter a valid option.")
 
 def customer():
+    clear_screen()
     print("Welcome to Relaxing Koala Restaurant!")
     # Ask if the customer has an existing order
     has_order = input("Do you have an existing order? (yes/no): ").strip().lower()
 
     if has_order == "yes":
+        clear_screen()
         order_id = input("Please enter your order ID: ")
         existing_order = OrderManagement.fetch_order_by_id(order_id)
         if existing_order:
@@ -103,10 +120,13 @@ def customer():
             while True:
                 action = input("Options: \n1. Track Order Status\n2. View Receipt\n3. Go back\nChoose an option: ")
                 if action == '1':
+                    clear_screen()
                     existing_order.display_order_statuses()
                 elif action == '2':
+                    clear_screen()
                     existing_order.display_invoice()
                 elif action == '3':
+                    clear_screen()
                     main()
                     break
                 else:
@@ -123,6 +143,7 @@ def customer():
         customer()  # Re-prompt the same question
 
 def choose_customer_type():
+    clear_screen()
     choice = input("Choose your service: \n1. Online\n2. Dine-in\n3. Go back\n")
 
     if choice == "1":
@@ -137,6 +158,7 @@ def choose_customer_type():
 
 
 def online_customer_process():
+    clear_screen()
     print("Welcome to Online Service")
     choice = input("Select an option:\n1. Make reservation\n2. Order food for delivery\n3. Go back\n")
 
@@ -151,6 +173,7 @@ def online_customer_process():
         online_customer_process()
 
 def dine_in_customer_process():
+    clear_screen()
     print("Welcome to Dine-in Service")
 
     customer_name = input("Please enter your name: ")
@@ -166,17 +189,19 @@ def dine_in_customer_process():
 
     # Create a dine-in order for the customer
     dine_in_order = factory.get_customer("dine_in", menu, customer_name, table_id, kitchen)
-    # Display the menu
-    menu.display_menu()
+    
     # Add or remove items from the cart
     while True:
         action = input("Enter 'A' to add item, 'R' to remove item, or '0' to finish: ").upper()
         if action == '0':
             break
         elif action == 'A':
+            # Display the menu
+            menu.display_menu()
             item_id = int(input("Enter item ID to add to cart: "))
             quantity = int(input(f"Enter quantity for item {item_id}: "))
             special_request = input("Any special requests? (press enter to skip): ")
+            clear_screen()
             dine_in_order.order.add_item_to_cart(item_id, quantity, menu, special_request)
         elif action == 'R':
             if not dine_in_order.order.cart:
@@ -187,16 +212,18 @@ def dine_in_customer_process():
                 dine_in_order.order.remove_item_from_cart(item_id, quantity)
         else:
             print("Invalid choice. Please enter 'A' to add, 'R' to remove, or '0' to finish.")
-
+            
+    clear_screen()
     # Send the order to the kitchen
     dine_in_order.order.send_to_kitchen(kitchen)
     online_system.add_order(dine_in_order.order)
     print("Order successfully sent to kitchen.")
-
+    
     # After sending the order to the kitchen, ask the customer if they want to view the invoice or go back to main menu
     while True:
         action = input("Select an option: \n1. View Invoice\n2. Go back to Main Menu\n")
         if action == '1':
+            clear_screen()
             dine_in_order.order.display_invoice()
         elif action == '2':
             main()
@@ -205,6 +232,7 @@ def dine_in_customer_process():
             print("Invalid choice. Please enter '1' to view the invoice or '2' to go back to the main menu.")
 
 def make_reservation():
+    clear_screen()
     print("Making a reservation:")
     while True:
         name = input("Please enter your name: ")
@@ -216,6 +244,7 @@ def make_reservation():
             online_customer = Reservation(name, date, time, guests)
             success = online_customer.make_reservation(name, date, time, guests)
             if success:
+                clear_screen()
                 print("Reservation made successfully.")
             else:
                 print("Failed to make reservation. Please try again.")
@@ -230,14 +259,12 @@ def make_reservation():
     main()
 
 def order_food_for_delivery():
+    clear_screen()
     print("Ordering food for delivery:")
 
     customer_name = input("Enter your name, phone number, and adress: ")
     # Create a delivery order for the customer
     delivery_order = factory.get_customer("delivery", menu, customer_name, None, kitchen)
-
-    # Display the menu
-    menu.display_menu()
 
     # Add or remove items from the cart
     while True:
@@ -245,9 +272,12 @@ def order_food_for_delivery():
         if action == '0':
             break
         elif action == 'A':
+            # Display the menu
+            menu.display_menu()
             item_id = int(input("Enter item ID to add to cart: "))
             quantity = int(input(f"Enter quantity for item {item_id}: "))
             special_request = input("Any special requests? (press enter to skip): ")
+            clear_screen()
             delivery_order.order.add_item_to_cart(item_id, quantity, menu, special_request)
         elif action == 'R':
             if not delivery_order.order.cart:
@@ -259,15 +289,19 @@ def order_food_for_delivery():
         else:
             print("Invalid choice. Please enter 'A' to add, 'R' to remove, or '0' to finish.")
 
+    clear_screen()
     # Make payment
     delivery_order.make_payment() 
 
     # After payment, ask the customer if they want to view the invoice or go back to main menu
     while True:
+        clear_screen()
         action = input("Select an option: \n1. View Receipt\n2. Track Order Status\n3. Go back to Main Menu\n")
         if action == '1':
+            clear_screen()
             delivery_order.view_receipt()  # Correct method to generate and view the receipt
         elif action == '2':
+            clear_screen()
             delivery_order.track_order_status()
         elif action == '3':
             main()
